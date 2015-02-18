370afefdc9f2332881dfb2a58f3c6a66fc376230
Soren Peterson <soren468@gmail.com>
2015-02-18 09:49:28 -0800

    Create routes for users. Create index controller. Create header view and add to default layout. Create basic index view.

---
+++ app/controllers/user.rb
@@ -0,0 +1,19 @@
+# No use
+get '/users' do
+end
+
+# Display a users homepage
+get '/users/:user_name' do
+end
+
+# Form to create a new user
+get '/users/new' do
+end
+
+# Create a new user
+post '/users' do
+end
+
+# Update User Info
+get '/users/:user_name/edit' do
+end
